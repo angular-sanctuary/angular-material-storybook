41472657e55df4ef3643979a83045ef733f385a8
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-12-23 16:21:16 +0100

    chore: update to angular 15

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,11 +1,5 @@
-import favicon from '../stories/assets/icons/favicon.ico';
 import { addons } from '@storybook/addons';
 import theme from './theme';
-
-const link = document.createElement('link');
-link.setAttribute('rel', 'shortcut icon');
-link.setAttribute('href', favicon);
-document.head.appendChild(link);
 
 addons.setConfig({
   theme: theme,
